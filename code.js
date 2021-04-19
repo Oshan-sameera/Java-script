386a7afc1b62a94a3ad50f6504a32a462fb85d68
oshan-sameera <oshan951@gmail.com>
2021-04-20 00:39:35 +1000

    fist-commit

---
+++ code.js
@@ -0,0 +1,10 @@
+//alert("hey");
+// document.write("<h1>Hellow WOrld</h>");
+// document.write("<\hr>");
+// document.write("<p> This is javascript tutorial<\p>");
+// console.log("Logging to the console");
+
+
+// var name = window.prompt("Enter Ypur name");
+// alert("your name is"+ name);
+
